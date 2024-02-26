747822868c3da20884df91f66dd27c1c362f1925
ICS4U-0 2023 <134095878+ics4u-0-2023@users.noreply.github.com>
2024-02-26 17:34:52 -0500

    test2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
-local opts = {}
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
