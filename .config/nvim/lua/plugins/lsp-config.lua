49397df4ec7ca475b9809e92f7a78e97fb82a956
ICS4U-0 2023 <134095878+ics4u-0-2023@users.noreply.github.com>
2024-03-03 14:33:47 -0500

    update

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -4,31 +4,25 @@ return {
     lazy = false,
     config = function()
       require("mason").setup()
-    end,
+    end
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
-    opts = {
-      auto_install = true,
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lau-ls"}
+      })
+    end
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
       
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-    end,
-  },
+      lspconfig.tsserver.setup({})
+      lspconfig.lua_ls.setup({})
+    end
+  }
 }
