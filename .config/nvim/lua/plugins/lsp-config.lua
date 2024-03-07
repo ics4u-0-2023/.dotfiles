6a90907cf050d43abc0aa011886bae78861c9a90
ICS4U-0-2023 <ics4u-0-2023@mths.ca>
2024-03-07 07:47:51 -0500

    fix spacing

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,57 @@
+<<<<<<< HEAD
 -- you will have to ensure npm and typescript are installed globally
 --
 -- sudo apt install nodejs
 -- sudo apt install npm
 -- sudo npm i -g typescript-language-server; sudo npm i -g typescript
 
+=======
+>>>>>>> ed69a90 (fix spacing)
 return {
   {
     "williamboman/mason.nvim",
     lazy = false,
     config = function()
       require("mason").setup()
+<<<<<<< HEAD
     end
+=======
+    end,
+>>>>>>> ed69a90 (fix spacing)
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     config = function()
       require("mason-lspconfig").setup({
+<<<<<<< HEAD
         ensure_installed = {"lua_ls", "tsserver", "jdtls"},
       })
     end
+=======
+        ensure_installed = {"lua_ls", "jdtls"},
+      })
+    end,
+>>>>>>> ed69a90 (fix spacing)
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
+<<<<<<< HEAD
       
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+=======
+
+      lspconfig.lua_ls.setup({})
+>>>>>>> ed69a90 (fix spacing)
       lspconfig.jdtls.setup({})
     end
   }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> ed69a90 (fix spacing)
