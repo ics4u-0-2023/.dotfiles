ec38a7f26f7f5f16f9c38dadd01fb2c00d8560e2
ICS4U-0-2023 <ics4u-0-2023@mths.ca>
2024-03-20 12:58:23 -0400

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,10 @@
+-- luacheck: ignore vim
 return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
